edb1316bdf7fa89db685eb01fccd5639614b0a98
Tim Bruce <tim@tjbruce.net>
2026-01-23 17:24:47 -0500

    Updated Player and Generator components. Removed player from Scene. Added dynamically set and placed player.

--- Generator/generator_node.gd
+++ Generator/generator_node.gd
@@ -14,6 +14,7 @@ var wall_vector: Vector2i
 var floor_vector: Vector2i
 var filler_vector: Vector2i
 var exit_vector: Vector2i
+var wall_tile_quadrant_size: int = 0
 
 var room_listing: Dictionary
 
@@ -47,6 +48,7 @@ func _ready() -> void:
 	
 func _get_main_tiles() -> void:
 	var terrains = {}
+	wall_tile_quadrant_size = tile_map_layer.rendering_quadrant_size
 	for terrain in tile_map_layer.tile_set.get_terrains_count(0):
 		terrains[str(terrain)] = tile_map_layer.tile_set.get_terrain_name(0,terrain)
 	var source = tile_map_layer.tile_set.get_source(0)
@@ -63,6 +65,13 @@ func _get_main_tiles() -> void:
 					filler_vector = source.get_tile_id(tile)
 				"Exit":
 					exit_vector = source.get_tile_id(tile)
+	
+
+func get_player_start() -> Vector2i:
+	var start_location: Vector2i
+	start_location = Vector2i(randi_range(room_listing["y"] + wall_tile_quadrant_size, (room_listing["width"] - 1) * wall_tile_quadrant_size),
+		randi_range(room_listing["x"] + wall_tile_quadrant_size, (room_listing["height"] - 1) * wall_tile_quadrant_size ))
+	return start_location
 
 
 func _generate_room(grid_location) -> Dictionary:
@@ -96,3 +105,5 @@ func _draw_rooms() -> void:
 				tile_map_layer.set_cell(Vector2i(x,y), 0, wall_vector)
 			elif y == room_listing["y"] || y == (room_listing["y"] + room_listing["height"]):
 				tile_map_layer.set_cell(Vector2i(x,y), 0, wall_vector)
+			else:
+				tile_map_layer.set_cell(Vector2i(x,y), 0, floor_vector)
